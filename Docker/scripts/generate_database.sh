336511617800942f96dc403ac3c144b2523cc4be
TomAIautomacao <pedro@nevesmkt.com>
2025-03-22 23:22:06 +0000

    Update generate_database.sh

--- Docker/scripts/generate_database.sh
+++ Docker/scripts/generate_database.sh
@@ -3,30 +3,29 @@
 source ./Docker/scripts/env_functions.sh
 
 if [ "$DOCKER_ENV" != "true" ]; then
-    export_env_vars
+  export_env_vars
 fi
 
 if [[ "$DATABASE_PROVIDER" == "postgresql" || "$DATABASE_PROVIDER" == "mysql" ]]; then
-    echo "🔧 Gerando banco de dados para: $DATABASE_PROVIDER"
+  echo "🚀 Gerando banco de dados para: $DATABASE_PROVIDER"
+  echo "🔗 DATABASE_URL: $DATABASE_URL"
 
-    echo "🔗 DATABASE_URL: $DATABASE_URL"
-
-    npx prisma generate --schema=prisma/schema.prisma
-
-    if [ $? -ne 0 ]; then
-        echo "❌ Falha ao rodar prisma generate"
-        exit 1
-    fi
-
-    npx prisma migrate deploy --schema=prisma/schema.prisma
-
-    if [ $? -ne 0 ]; then
-        echo "❌ Falha ao rodar prisma migrate"
-        exit 1
-    else
-        echo "✅ Migrate executado com sucesso!"
-    fi
-else
-    echo "❌ DATABASE_PROVIDER inválido: $DATABASE_PROVIDER"
+  # Gera o Prisma Client
+  npx prisma generate
+  if [ $? -ne 0 ]; then
+    echo "❌ Falha ao rodar prisma generate"
     exit 1
+  fi
+
+  # Aplica as migrations no banco
+  npx prisma migrate deploy
+  if [ $? -ne 0 ]; then
+    echo "❌ Falha ao rodar prisma migrate"
+    exit 1
+  fi
+
+  echo "✅ Prisma setup finalizado com sucesso!"
+else
+  echo "❌ DATABASE_PROVIDER inválido: $DATABASE_PROVIDER"
+  exit 1
 fi
